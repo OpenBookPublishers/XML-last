736b1e8136e5ef4a4099c8c364741e8d711cd4e9
Javier Arias <javier@arias.re>
2019-12-11 10:52:51 +0000

    generalise ignores

--- XML-before-transformation.py
+++ XML-before-transformation.py
@@ -3,6 +3,11 @@ import os.path
 import shutil
 from zipfile import ZipFile
 
+
+IGNORE_XHTML = shutil.ignore_patterns('*.xhtml')
+IGNORE_SECONDARY = shutil.ignore_patterns('*.opf', 'back*', '*cover*',
+                                          'half*', 'toc*', 'css', 'font')
+
 # read all files in the transformation folder
 folder = os.path.dirname(os.path.abspath(__file__))
 foldercontent = os.listdir(folder)
@@ -41,29 +46,35 @@ extract(newepubname, intermediatefolder)
 oebpsfolder = intermediatefolder+'/OEBPS'
 inputfolder = folder+'/input'
 if not os.path.exists(inputfolder):
-    shutil.copytree(oebpsfolder, inputfolder, symlinks=False, ignore=shutil.ignore_patterns('*.opf', 'back*', '*cover*', 'half*', 'toc*', 'css', 'font'))
+    shutil.copytree(oebpsfolder, inputfolder, symlinks=False,
+                    ignore=IGNORE_SECONDARY)
 else:
     shutil.rmtree(inputfolder)
-    shutil.copytree(oebpsfolder, inputfolder, symlinks=False, ignore=shutil.ignore_patterns('*.opf', 'back*', '*cover*', 'half*', 'toc*', 'css', 'font'))
+    shutil.copytree(oebpsfolder, inputfolder, symlinks=False,
+                    ignore=IGNORE_SECONDARY)
 
-# copy images from input folder to XML folder
+# copy images from input folder to XML folIGNORE_XHTMLder
 imagefolder = folder+'/input/image'
 XMLfolder = folder+'/XML-edition/image'
 if not os.path.exists(XMLfolder):
-    shutil.copytree(imagefolder, XMLfolder, symlinks=False, ignore=shutil.ignore_patterns('*.xhtml'))
+    shutil.copytree(imagefolder, XMLfolder, symlinks=False,
+                    ignore=IGNORE_XHTML)
 else:
     shutil.rmtree(XMLfolder)
-    shutil.copytree(imagefolder, XMLfolder, symlinks=False, ignore=shutil.ignore_patterns('*.xhtml'))
+    shutil.copytree(imagefolder, XMLfolder, symlinks=False,
+                    ignore=IGNORE_XHTML)
 
 # copy audio from input folder to XML folder
 audiofolder = folder+'/input/audio'
 XMLaudiofolder = folder+'/XML-edition/audio'
 if os.path.exists(audiofolder):
     if not os.path.exists(XMLaudiofolder):
-        shutil.copytree(audiofolder, XMLaudiofolder, symlinks=False, ignore=shutil.ignore_patterns('*.xhtml'))
+        shutil.copytree(audiofolder, XMLaudiofolder, symlinks=False,
+                        ignore=IGNORE_XHTML)
     else:
         shutil.rmtree(XMLaudiofolder)
-        shutil.copytree(audiofolder, XMLaudiofolder, symlinks=False, ignore=shutil.ignore_patterns('*.xhtml'))
+        shutil.copytree(audiofolder, XMLaudiofolder, symlinks=False,
+                        ignore=IGNORE_XHTML)
 
 
 # copy videos from input folder to XML folder
@@ -71,7 +82,9 @@ videofolder = folder+'/input/video'
 XMLvideofolder = folder+'/XML-edition/video'
 if os.path.exists(videofolder):
     if not os.path.exists(XMLvideofolder):
-        shutil.copytree(videofolder, XMLvideofolder, symlinks=False, ignore=shutil.ignore_patterns('*.xhtml'))
+        shutil.copytree(videofolder, XMLvideofolder, symlinks=False,
+                        ignore=IGNORE_XHTML)
     else:
         shutil.rmtree(XMLvideofolder)
-        shutil.copytree(videofolder, XMLvideofolder, symlinks=False, ignore=shutil.ignore_patterns('*.xhtml'))
+        shutil.copytree(videofolder, XMLvideofolder, symlinks=False,
+                        ignore=IGNORE_XHTML)
